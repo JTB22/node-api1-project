9ba4c4ddd504e45326fc1d8feb4cc04ab5a4ae86
Josh Blood <jblood92@pm.me>
2023-09-07 16:24:24 -0400

    get user and user by id. post new user working

--- api/server.js
+++ api/server.js
@@ -1,3 +1,53 @@
 // BUILD YOUR SERVER HERE
+const express = require("express");
+const server = express();
+const Users = require("./users/model");
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+server.use(express.json());
+
+server.get("/api/users", async (req, res) => {
+  let users = await Users.find();
+  try {
+    res.status(200).json(users);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "The users information could not be retrieved" });
+  }
+});
+
+server.get("/api/users/:id", async (req, res) => {
+  let user = await Users.findById(req.params.id);
+  try {
+    if (!user) {
+      res
+        .status(404)
+        .json({ message: "The user with the specified ID does not exist" });
+    } else {
+      res.status(200).json(user);
+    }
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "The user information could not be retrieved" });
+  }
+});
+
+server.post("/api/users", async (req, res) => {
+  let newUser = await Users.insert(req.body);
+  try {
+    if (!req.body.name || !req.body.bio) {
+      res
+        .status(400)
+        .json({ message: "Please provide name and bio for the user" });
+    } else {
+      res.status(201).json(newUser);
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "There was an error while saving the user to the database",
+    });
+  }
+});
+
+module.exports = server;
