7b48a4d0054de01b72115264c03045bda1b657b0
Josh Blood <jblood92@pm.me>
2023-09-07 16:29:03 -0400

    delete and put working, test passing

--- api/server.js
+++ api/server.js
@@ -50,4 +50,40 @@ server.post("/api/users", async (req, res) => {
   }
 });
 
+server.put("/api/users/:id", async (req, res) => {
+  let updatedUser = await Users.update(req.params.id, req.body);
+  try {
+    if (!updatedUser) {
+      res
+        .status(404)
+        .json({ message: "The user with the specified ID does not exist" });
+    } else if (!req.body.name || !req.body.bio) {
+      res
+        .status(400)
+        .json({ message: "Please provide name and bio for the user" });
+    } else {
+      res.status(200).json(updatedUser);
+    }
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "The user information could not be modified" });
+  }
+});
+
+server.delete("/api/users/:id", async (req, res) => {
+  let deletedUser = await Users.remove(req.params.id);
+  try {
+    if (!deletedUser) {
+      res
+        .status(404)
+        .json({ message: "The user with the specified ID does not exist" });
+    } else {
+      res.status(200).json(deletedUser);
+    }
+  } catch (err) {
+    res.status(500).json({ message: "The user could not be removed" });
+  }
+});
+
 module.exports = server;
